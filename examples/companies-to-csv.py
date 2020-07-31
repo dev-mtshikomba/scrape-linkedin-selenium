b94de62961f341934435596ec4640a4d241534d9
Matheus Shikomba <mshikomba@duosecurity.com>
2020-07-31 15:16:14 -0400

    example updated

--- examples/companies-to-csv.py
+++ examples/companies-to-csv.py
@@ -8,12 +8,13 @@ my_company_list = ['facebook', 'linkedin']
 
 company_data = []
 
-with CompanyScraper() as scraper:
+with CompanyScraper(timeout=180, cookie='AQEFALoBAAAAAAOTYYwAAAFzjoGKQgAAAXOyjg5CVgAAXXVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjIyMTEwOTAsMTA0NzY0MTk1KV51cm46bGk6bWVtYmVyOjM0NjY2ODIwNsCH3CVUAtUsah') as scraper:
     # Get each company's overview, add to company_data list
     for name in my_company_list:
         overview = scraper.scrape(company=name).overview
-        overview['company_name'] = name
-        company_data.append(overview)
+        if (overview != None):
+            overview['company_name'] = name
+            company_data.append(overview)
 
 # Turn into dataframe for easy csv output
 df = pd.DataFrame(company_data)
